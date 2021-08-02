2256c8c93408ae14dcd2a4fe88a7862232e3e720
dude lao <brycelipscomb@gmail.com>
2021-08-02 03:14:43 -0400

    added form and hook for TextField component

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -1,9 +1,26 @@
 import {useState} from 'react';
 
+const initialValues = {
+    text: ""
+}
+
 export default function TextField(){
+    const [state, setState] = useState(initialValues)
+
+    const handleChange = e => {
+        return setState({
+            ...state,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return(
         <div>
             This is where you will input all of your text
+            <form>
+                <input type="text" name="text" value={state.text} onChange={handleChange} placeholder="Enter your message here..." required maxLength="160" size="100" />
+                <button type="submit" >Send</button>
+            </form>
         </div>
     )
 }
