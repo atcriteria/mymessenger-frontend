55f9c964f6f00995af855fc9348e8d88dce665e8
dude lao <brycelipscomb@gmail.com>
2021-08-03 20:59:42 -0400

    configured the message component to be dynamic

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,11 @@
 // A message component to house displayed messages
 // from the ChatBox.js component
 
-export default function Message({message}){
+export default function Message({messageObject}){
     return(
         <div className="message-wrapper">
-            <h3>UsernameHere</h3>
-            <p>{(message) ? `${message}` : "This is a bunch of random text that I am manually typing because I forgot the shortcut to inject lorem ipsum text in here. Hopefully this is enough to help me get an idea for how to set the message box width and taxt wrapping. lalalal let's see what happens."}</p>
+            <h3>{messageObject.username}</h3>
+            <p>{messageObject.message}</p>
         </div>
     )
 }
