de584a81589685acd35866ece5e2f1a962d4a848
dude lao <brycelipscomb@gmail.com>
2021-08-05 05:08:06 -0400

    removed old console.log

--- src/util/socket.js
+++ src/util/socket.js
@@ -8,13 +8,11 @@ socket.on("connect", () => {
     const username = socket.auth.username;
     const message = `${username} has connected.`
     socket.emit("send-admin-message", {adminMessage: true, message: message})
-    console.log(username)
 })
 socket.on("disconnect", () => {
     const username = socket.auth.username
     const message = `${username} left the chat.`
     socket.emit("send-admin-message", {adminMessage: true, message: message})
-    console.log(username)
 })
 
 export {
