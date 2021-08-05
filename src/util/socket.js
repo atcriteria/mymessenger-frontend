376f1df0615e4665f1d21ff8a1d5434c05ce3430
dude lao <brycelipscomb@gmail.com>
2021-08-05 05:29:28 -0400

    using hosted url for debugging

--- src/util/socket.js
+++ src/util/socket.js
@@ -1,7 +1,7 @@
 import {io} from 'socket.io-client';
 
 // creating instance of socket.io pointing at our server
-const URL = process.env.PROD_SERVER || 'http://localhost:3333';
+const URL = "https://backend-for-bad-messenger.herokuapp.com";
 const socket = io(URL, { autoConnect: false});
 
 socket.on("connect", () => {
