ed4ca5162701f0690c34bd78c075cb630cde3c4d
dude lao <brycelipscomb@gmail.com>
2021-08-05 04:38:31 -0400

    style updates

--- src/components/UserWindow.js
+++ src/components/UserWindow.js
@@ -4,9 +4,11 @@ import TextField from "./TextField";
 export default function UserWindow({username, colorScheme, filterLanguage}){
     return(
         <div className="user-window-wrapper">
-            <h2>You are registered as <span className="username-span">{username}</span></h2>
-            <ChatBox username={username} colorScheme={colorScheme} filterLanguage={filterLanguage} />
-            <TextField username={username} colorScheme={colorScheme} />
+            <div className="user-window-content-wrapper">
+                <h2 className="user-title">You are registered as <span className="username-span">{username}</span></h2>
+                <ChatBox username={username} colorScheme={colorScheme} filterLanguage={filterLanguage} />
+                <TextField username={username} colorScheme={colorScheme} />
+            </div>
         </div>
     )
 }
